a606703e18c6b898164d4e423d050c9058763aee
Muhammad Yafi <yafithekid212@gmail.com>
2015-04-04 11:59:57 +0700

    change sidebar layouts

--- resources/views/layouts/sidebar/pengguna.blade.php
+++ resources/views/layouts/sidebar/pengguna.blade.php
@@ -1,1 +1,17 @@
-user cok
+<div class='sidebar panel panel-info'>
+    <div class='panel-heading'>
+        <h3 class='panel-title'>Menu Izin</h3>
+    </div>
+    <ul class='list-group'>
+        <a href='#'><li class='list-group-item'>Daftar Permohonan Izin</li></a>
+    </ul>
+</div>
+
+<div class='sidebar panel panel-info'>
+    <div class='panel-heading'>
+        <h3 class='panel-title'>Menu Trayek</h3>
+    </div>
+    <ul class='list-group'>
+        <a href='#'><li class='list-group-item'>Rekomendasi Trayek</li></a>
+    </ul>
+</div>
